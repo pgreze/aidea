9ef3cb1f7e86c7dbb37c80e0824db857704cb0bd
pgreze <pierrick.greze@gmail.com>
2022-10-16 18:29:43 +0900

    Fix optional parameter

--- app/src/main/kotlin/com/github/pgreze/aidea/app/App.kt
+++ app/src/main/kotlin/com/github/pgreze/aidea/app/App.kt
@@ -2,7 +2,7 @@ package com.github.pgreze.aidea.app
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
-import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.arguments.optional
 import com.github.ajalt.clikt.parameters.types.file
 import com.github.pgreze.aidea.idea.listIdeaInstallations
 import java.io.File
@@ -13,8 +13,9 @@ fun main(args: Array<String>) {
 
 class App : CliktCommand() {
 
-    private val target: File? by option(help = "File or folder to open an IDEA based IDE with.")
+    private val target: File? by argument(help = "File or folder to open an IDEA based IDE with.")
         .file(mustExist = true)
+        .optional()
 
     private val ideaInstalls by lazy {
         listIdeaInstallations().sortedByDescending { it.installDir }
